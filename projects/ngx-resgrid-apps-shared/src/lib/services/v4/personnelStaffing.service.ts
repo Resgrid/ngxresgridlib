fc7f1987e7f61cf8d21d35d68671eea2636b339f
Shawn Jackson <shawn@resgrid.com>
2022-04-14 13:21:08 -0700

    fix: fixing staffing api objs (#10)
    * Update release.config.js
    
    Release action updates
    
    * Update release.config.js
    
    feat: working on release script
    
    * fix: supporting multiple versions of angular
    
    * fix: Added mobile scope.
    
    * fix: Added missing param
    
    * fix: Fixing broken test
    
    * fix: added calls for custom states
    
    * fix: added personnel status api
    
    * fix: adding missing public apis
    
    * fix: added personnel staffing api
    
    * fix: personnel staffing value fix

--- projects/ngx-resgrid-apps-shared/src/lib/services/v4/personnelStaffing.service.ts
+++ projects/ngx-resgrid-apps-shared/src/lib/services/v4/personnelStaffing.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ResgridConfig } from '../../resgrid-config';
+import { SavePersonsStaffingsInput } from '../../models/v4/personnelStaffing/savePersonsStaffingsInput';
+import { SavePersonsStaffingsResult } from '../../models/v4/personnelStaffing/savePersonsStaffingsResult';
+import { SavePersonStaffingInput } from '../../models/v4/personnelStaffing/savePersonStaffingInput';
+import { SavePersonStaffingResult } from '../../models/v4/personnelStaffing/savePersonStaffingResult';
 import { SavePersonStatusInput } from '../../models/v4/personnelStatuses/savePersonStatusInput';
 import { SavePersonStatusResult } from '../../models/v4/personnelStatuses/savePersonStatusResult';
 import { SavePersonsStatusesInput } from '../../models/v4/personnelStatuses/savePersonsStatusesInput';
@@ -17,23 +21,15 @@ export class PersonnelStaffingService {
     private config: ResgridConfig
   ) {}
 
-  public savePersonStatus(data: SavePersonStatusInput): Observable<SavePersonStatusResult> {
+  public savePersonStatus(data: SavePersonStaffingInput): Observable<SavePersonStaffingResult> {
     const url = this.config.apiUrl + '/PersonnelStaffing/SavePersonStaffing';
 
-    if (!data.RespondingTo) {
-      data.RespondingTo = '0';
-    }
-
-    return this.http.post<SavePersonStatusResult>(url, data);
+    return this.http.post<SavePersonStaffingResult>(url, data);
   }
 
-  public savePersonsStatuses(data: SavePersonsStatusesInput): Observable<SavePersonsStatusesResult> {
+  public savePersonsStatuses(data: SavePersonsStaffingsInput): Observable<SavePersonsStaffingsResult> {
     const url = this.config.apiUrl + '/PersonnelStaffing/SavePersonsStaffings';
 
-    if (!data.RespondingTo) {
-      data.RespondingTo = '0';
-    }
-
-    return this.http.post<SavePersonsStatusesResult>(url, data);
+    return this.http.post<SavePersonsStaffingsResult>(url, data);
   }
 }
