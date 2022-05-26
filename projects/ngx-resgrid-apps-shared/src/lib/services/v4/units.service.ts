54d002c9a7a084f71a6804ab98e7d71aba36f436
Shawn Jackson <shawn@resgrid.com>
2022-05-25 21:11:21 -0700

    fix: added unit filtering (#19)

--- projects/ngx-resgrid-apps-shared/src/lib/services/v4/units.service.ts
+++ projects/ngx-resgrid-apps-shared/src/lib/services/v4/units.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ResgridConfig } from '../../resgrid-config';
 import { UnitsResult } from '../../models/v4/units/unitsResult';
+import { GetUnitFilterOptionsResult } from '../../models/v4/units/getUnitFilterOptionsResult';
+import { UnitsInfoResult } from '../../models/v4/units/unitInfoResult';
 
 
 @Injectable({
@@ -17,5 +19,22 @@ export class UnitsService {
   public getAllUnits(): Observable<UnitsResult> {
     const url = this.config.apiUrl + `/Units/GetAllUnits`;
     return this.http.get<UnitsResult>(url);
+  }
+
+  public getAllUnitsInfos(filter: string): Observable<UnitsInfoResult> {
+    let url = '';
+    
+    if (filter) {
+      url = this.config.apiUrl + `/Units/GetAllUnitsInfos?activeFilter=${encodeURIComponent(filter)}`;
+    } else {
+      url = this.config.apiUrl + `/Units/GetAllUnitsInfos`;
+    }
+    
+    return this.http.get<UnitsInfoResult>(url);
+  }
+
+  public getUnitsFilterOptions(): Observable<GetUnitFilterOptionsResult> {
+    const url = this.config.apiUrl + `/Units/GetUnitsFilterOptions`;
+    return this.http.get<GetUnitFilterOptionsResult>(url);
   }
 }
