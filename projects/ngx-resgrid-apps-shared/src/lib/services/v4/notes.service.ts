3426547c8e396e014b153b70b7becbfb9483a6fa
Shawn Jackson <shawn@resgrid.com>
2022-06-08 17:27:04 -0700

    fix: added helpers and dispatch note

--- projects/ngx-resgrid-apps-shared/src/lib/services/v4/notes.service.ts
+++ projects/ngx-resgrid-apps-shared/src/lib/services/v4/notes.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ResgridConfig } from '../../resgrid-config';
-import { RolesResult } from '../../models/v4/roles/rolesResult';
 import { NotesResult } from '../../models/v4/notes/notesResult';
+import { NoteResult } from '../../models/v4/notes/noteResult';
 
 
 @Injectable({
@@ -18,5 +18,10 @@ export class NotesService {
   public getAllNotes(): Observable<NotesResult> {
     const url = this.config.apiUrl + '/Notes/GetAllNotes';
     return this.http.get<NotesResult>(url);
+  }
+
+  public getDispatchNote(): Observable<NoteResult> {
+    const url = this.config.apiUrl + '/Notes/GetDispatchNote';
+    return this.http.get<NoteResult>(url);
   }
 }
