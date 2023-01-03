e16cd0b4333d13fd9d8bf7f09236323220704d2b
Shawn Jackson <shawn@resgrid.com>
2023-01-03 12:47:11 -0800

    fix: add download for files (#71)

--- projects/ngx-resgrid-apps-shared/src/lib/services/v4/callProtocols.service.ts
+++ projects/ngx-resgrid-apps-shared/src/lib/services/v4/callProtocols.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ResgridConfig } from '../../resgrid-config';
 import { CallProtocolsResult } from '../../models/v4/callProtocols/callProtocolsResult';
@@ -17,5 +17,16 @@ export class CallProtocolsService {
   public getAllCallProtocols(): Observable<CallProtocolsResult> {
     const url = this.config.apiUrl + '/CallProtocols/GetAllCallProtocols';
     return this.http.get<CallProtocolsResult>(url);
+  }
+
+  public getCallProtocolAttachmentFile(attachmentId: string): Observable<any> {
+    const url = this.config.apiUrl + '/CallProtocols/GetFile?protocolAttachmentId=' + attachmentId;
+
+    const requestOptions: Object = {
+      responseType: 'blob',
+      reportProgress: true,
+      observe: 'events'
+    }
+    return this.http.get<any>(url, requestOptions);
   }
 }
