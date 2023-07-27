3a9e4d810b2d4ec03f7b210d5a718de9ea862b8a
Shawn Jackson <shawn@resgrid.com>
2023-07-27 16:56:52 -0700

    fix: adding audio stream type (#79)

--- projects/ngx-resgrid-apps-shared/src/lib/models/v4/voice/departmentAudioResultStreamData.ts
+++ projects/ngx-resgrid-apps-shared/src/lib/models/v4/voice/departmentAudioResultStreamData.ts
@@ -1,5 +1,6 @@
 export class DepartmentAudioResultStreamData {
     public Id: string = '';
-    public Name: string = ''; 
+    public Name: string = '';
+    public Type: string = '';
     public Url: string = '';
 }
