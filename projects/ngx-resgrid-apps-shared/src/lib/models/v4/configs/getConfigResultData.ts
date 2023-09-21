430592f70e9602506078448c1e4bb2c45df76812
Shawn Jackson <shawn@resgrid.com>
2023-09-21 09:09:39 -0700

    fix: adding config items (#86)

--- projects/ngx-resgrid-apps-shared/src/lib/models/v4/configs/getConfigResultData.ts
+++ projects/ngx-resgrid-apps-shared/src/lib/models/v4/configs/getConfigResultData.ts
@@ -1,6 +1,8 @@
 export class GetConfigResultData  {
     public W3WKey: string = '';
     public GoogleMapsKey: string = '';
+    public DirectionsMapKey: string = '';
+    public NavigationMapKey: string = '';
     public LoggingKey: string = '';
     public MapUrl: string = '';
     public MapAttribution: string = '';
