5559b882ce8da472061eae45e03877ead06ef754
Shawn Jackson <shawn@resgrid.com>
2024-02-23 11:23:35 -0800

    fix: unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -33,23 +33,4 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   });
-
-  //it('should create the app', () => {
-  //  const fixture = TestBed.createComponent(AppComponent);
-  //  const app = fixture.componentInstance;
-  //  expect(app).toBeTruthy();
-  //});
-
-  //it(`should have as title 'ResgridAppShared'`, () => {
-  //  const fixture = TestBed.createComponent(AppComponent);
-  //  const app = fixture.componentInstance;
-  //  expect(app.title).toEqual('ResgridAppShared');
-  //});
-
-  //it('should render title', () => {
-  //  const fixture = TestBed.createComponent(AppComponent);
-  //  fixture.detectChanges();
-  //  const compiled = fixture.nativeElement;
-  //  expect(compiled.querySelector('.content span').textContent).toContain('ResgridAppShared app is running!');
-  //});
 });
