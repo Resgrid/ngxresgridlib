3af7ee7e5f8d99fbc015cc0b764f05f7c3bfed4b
Shawn Jackson <shawn@resgrid.com>
2024-02-22 21:06:21 -0800

    fix: tests again (#94)

--- projects/ngx-resgrid-apps-shared/src/test.ts
+++ projects/ngx-resgrid-apps-shared/src/test.ts
@@ -20,3 +20,7 @@ getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting()
 );
+// Then we find all the tests.
+//const context = require.context('./', true, /\.spec\.ts$/);
+// And load the modules.
+//context.keys().map(context);
